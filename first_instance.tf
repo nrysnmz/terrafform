945cd3ee001c689d284709613e82bca8175523f9
Nuray Korganci <nrysnmz@gmail.com>
2023-12-05 04:02:14 -0600

    m

---
+++ first_instance.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIA27I7RI4IR7VXP47I"
+  secret_key = "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"
+}
+
+resource "aws_instance" "Mock-Service" {
+  ami = "ami-0230bd60aa48260c6"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Mock Service Computer"
+  }
+}
