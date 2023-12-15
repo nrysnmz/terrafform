bbf0ec6c57def8c97f333b8954443c9aedeeabe3
Nuray Korganci <nrysnmz@gmail.com>
2023-12-14 21:26:48 -0600

    m

--- first_instance.tf
+++ first_instance.tf
@@ -1,18 +1,10 @@
 
+provider "aws" {
+  region = "us-east-1"
 
-resource "aws_instance" "Mock-Service" {
-  ami = "ami-0230bd60aa48260c6"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "Mock Service Computer"
-  }
 }
 
-terraform {
-  required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
-    }
-  }
+resource "aws_iam_user" "demouser" {
+
+  name = "nuray"
 }
