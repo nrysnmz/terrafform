3395ca1ae4138c47d4fee3c8555b461b6d54cf0b
Nuray Korganci <nrysnmz@gmail.com>
2024-01-01 16:46:59 -0600

    m

--- first_instance.tf
+++ first_instance.tf
@@ -3,28 +3,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
+provider "azurerm" {
+  features {}
+}
+
 resource "aws_instance" "example_instance" {
   ami           = "ami-0759f51a90924c166"
   instance_type = "t2.micro"
-
-  provisioner "local-exec" {
-    command = "echo 'Hello, Terraform!'"
-  }
-
-
+  count         = 3
 }
-
-variable "prefix" {
-}
-
-variable "location" {
-}
-
-terraform {
-  backend "s3" {
-    bucket = "my-terraform-state-bucket"
-    key    = "path/to/my/terraform.tfstate"
-    region = "us-west-2"
-  }
-}
-
